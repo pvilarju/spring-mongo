2fdccee46a5e4322559244fda8e132d4abb62865
Paulo Roberto <paulovilarjnr@gmail.com>
2022-08-16 23:55:28 -0300

    entity user and rest work

---
+++ src/main/java/com/mega/springmongo/resource/UserResource.java
@@ -0,0 +1,24 @@
+package com.mega.springmongo.resource;
+
+import com.mega.springmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+
+        return ResponseEntity.ok().body(new ArrayList<>(Arrays.asList(maria, alex)));
+    }
+}
